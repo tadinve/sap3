f80e98a2cad53d43a639940b5129d6f2a6ed4e62
Venkatesh Tadinada <venkat@solivar.com>
2012-02-26 15:03:30 -0800

    Initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sap3::Application.config.secret_token = '379ecc1c84199c79ea3539c8f063ca676538cb585aa71a19555912e4e3c56eed8033fb545d7c02891f6dc981e00a467f1eed32177affe9ddbe3740041d97e807'
